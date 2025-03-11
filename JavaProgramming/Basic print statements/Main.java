b7a2b1015fae5d01d6348e4e92addef6a882453e
akshayanagiri <168463039+akshayanagiri@users.noreply.github.com>
2025-03-11 10:30:40 +0530

    Add files via upload

---
+++ JavaProgramming/Basic print statements/Main.java
@@ -0,0 +1,25 @@
+/* -> skeleton
+    -> print statement-hello world
+    -> printing next line - 2 types
+    -> " " , \ printing
+    -> one line and multi line comments
+*/
+
+public class Main{
+    public static void main (String [] args){
+    
+     
+    //basic print statement
+    System.out.print("hello world ");
+     //can print next line by using println or by using \n 
+    System.out.print("my name is Akshaya \n"); 
+    // \t for tab space
+    System.out.println("\t learing java language..");
+
+     //printing " " 
+    System.out.println("In programming world \"Java\" is one of the most popular language");
+    //printing backslash \
+    System.out.println("We generally use Java\\C++\\Python in MNC's");
+
+}
+}
